836e71da97f44426326b5e66dd6b2633065531e0
Thiago Roberto <thiago.nemo4code@gmail.com>
2019-12-05 21:55:06 -0300

    criando rota e controle update de produtos

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -17,5 +17,15 @@ module.exports = {
     const product = await Product.create(req.body);
 
     return res.json(product);
+  },
+  async update(req, res) {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    });
+
+    return res.json(product);
+  },
+  async destroy(req, res) {
+    //
   }
 };
